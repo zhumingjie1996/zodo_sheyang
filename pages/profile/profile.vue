e905e83b6e964875a2a138f30c51c89e1d598fad
zhumingjie1996 <1005614322@qq.com>
2023-10-14 00:21:31 +0800

    feat:新增“我的”页面，添加样式和布局

--- pages/profile/profile.vue
+++ pages/profile/profile.vue
@@ -1,22 +1,116 @@
 <template>
-	<view>
-		我的
+	<view class="profileContainer">
+		<view class="header">
+			<view class="header_left">
+				<view class="avatar">
+					<u-icon name="account-fill" size="50" color="#fff"></u-icon>
+				</view>
+				<view class="nameAndPhone">
+					<span class="userName">{{ userName }}</span>
+					<span class="userPhone">{{ userPhone }}</span>
+				</view>
+			</view>
+			<view class="header_right">
+				<u-icon name="arrow-right" size="30" color="#fff"></u-icon>
+			</view>
+		</view>
+		<view class="container">
+			<view class="cells">
+				<u-cell title="用户管理" border isLink size="large">
+					<template slot="icon">
+						<u-icon name="plus-people-fill" size="25"></u-icon>
+					</template>
+				</u-cell>
+			</view>
+			<view class="logOutBtn">退出登录</view>
+		</view>
 	</view>
 </template>
 
 <script>
-	export default {
-		data() {
-			return {
-				
-			}
-		},
-		methods: {
-			
+export default {
+	data() {
+		return {
+			userName: '13900000000',
+			userPhone: '13900000000'
 		}
+	},
+	methods: {
+
 	}
+}
 </script>
 
-<style>
+<style lang="scss" scoped>
+@import '@/uni.scss';
 
+.profileContainer {
+	position: relative;
+	width: 100vw;
+	height: 100vh;
+	background-color: $zodo-main-color-1;
+
+	.header {
+		position: absolute;
+		width: 100%;
+		height: 100px;
+		top: 15%;
+		transform: translateY(-50%);
+		padding: 0 20px;
+		box-sizing: border-box;
+
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
+
+		.header_left{
+			display: flex;
+			flex-direction: row;
+			align-items: center;
+			gap: 20px;
+
+			.avatar{
+				border: 2px solid #fff;
+				border-radius: 100%;
+				padding: 5px;
+			}
+
+			.nameAndPhone{
+				display: flex;
+				flex-direction: column;
+				justify-content: space-around;
+				gap: 10px;
+				color: #fff;
+			}
+		}
+	}
+
+	.container {
+		position: absolute;
+		bottom: 0;
+		left: 0;
+		height: 70%;
+		width: 100%;
+		background-color: #fff;
+		border-radius: 30px 30px 0 0;
+		padding: 30px 20px;
+		box-sizing: border-box;
+
+		.logOutBtn {
+			position: absolute;
+			bottom: 30px;
+			width: calc(100% - 40px);
+			height: 45px;
+			border-radius: 999px;
+			box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.1);
+			background-color: $zodo-main-color-1;
+			color: #fff;
+			line-height: 45px;
+			text-align: center;
+			font-weight: 700;
+			letter-spacing: $zodo-letter-space;
+		}
+	}
+}
 </style>
